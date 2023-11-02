23b1fff4f81f76f2ea11abe39c7a28db8e497445
Tim Tully <tully_tim@yahoo.com>
2023-11-02 08:29:21 -0700

    latest

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,21 @@ use futures::executor::block_on;
 use std::{error::Error, collections::HashMap, env};
 
 #[tokio::main]
- async fn test() { 
+async fn test() { 
 
     let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        println!("Usage: baseten <api_key>");
+    if args.len() < 3 {
+        println!("Usage: baseten <api_key> <model>");
         return;
     }
-    let  api_key = &args[1];
+    let api_key = &args[1];
+    let model  = &args[2];
 
     let baseten = Baseten {
         api_key: api_key.to_string()
     };
 
-    let model = String::from("yqe4MdP");
+    //let model = String::from("yqe4MdP");
     let prompt = String::from("A tree in a field under the night sky");
 
     let mut opts = HashMap::new();
