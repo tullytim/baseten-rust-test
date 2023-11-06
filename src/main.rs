5a921c74465b1097bd39a13413f887957676eabc
Tim Tully <tully_tim@yahoo.com>
2023-11-06 15:59:51 -0800

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ use std::{error::Error, collections::HashMap, env};
 
 #[tokio::main]
 async fn test() { 
-
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
         println!("Usage: baseten <api_key> <model>");
@@ -19,13 +18,13 @@ async fn test() {
         api_key: api_key.to_string()
     };
 
-    //let model = String::from("yqe4MdP");
     let prompt = String::from("A tree in a field under the night sky");
 
     let mut opts = HashMap::new();
     opts.insert(String::from("use_refiner"), String::from("true"));
 
     let r: Result<String, Box<dyn Error>> = block_on(baseten.call_model_prompt(&model, &prompt, Some(opts)));
+    //let r: Result<String, Box<dyn Error>> = block_on(baseten.wake(&model));
     match r {
         Ok(s) => println!("{}", s),
         Err(e) => println!("Error: {}", e),
